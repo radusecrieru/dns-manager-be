6f5661f380250ba56b794530479912aaef2d288d
Radu Secrieru <radusecrieru91@gmail.com>
2015-09-13 10:12:04 -0700

    Add Content-Type to CORS filter

--- src/main/java/com/radusecrieru/dns/manager/config/filter/CorsFilter.java
+++ src/main/java/com/radusecrieru/dns/manager/config/filter/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
         chain.doFilter(req, res);
     }
 
